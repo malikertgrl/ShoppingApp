276bfe5a484b4c54dc14418c25acc6927ff8417e
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2022-05-24 22:53:09 +0300

    svgs added

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Account from "../screens/Account"
 import List from "../screens/List"
+import Menu from "../assets/svgs/Vectormenu.svg"
+import Auth from "../assets/svgs/VectorAccount.svg"
 
 const Tab = createBottomTabNavigator();
 
@@ -11,8 +13,16 @@ export default function Router() {
     return (
         <NavigationContainer>
             <Tab.Navigator initialRouteName='Account' screenOptions={{ headerShown: false }}>
-                <Tab.Screen name="List" component={List} />
-                <Tab.Screen name="Account" component={Account} />
+                <Tab.Screen name="List" component={List} options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Menu width={24} height={24} />
+                    )
+                }} />
+                <Tab.Screen name="Account" component={Account} options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Auth width={24} height={24} />
+                    )
+                }} />
             </Tab.Navigator>
         </NavigationContainer>
     );
--- src/screens/List.js
+++ src/screens/List.js
@@ -1,53 +1,11 @@
-import Svg, {
-    Circle,
-    Ellipse,
-    G,
-    TSpan,
-    TextPath,
-    Path,
-    Polygon,
-    Polyline,
-    Line,
-    Rect,
-    Use,
-    Image,
-    Symbol,
-    Defs,
-    LinearGradient,
-    RadialGradient,
-    Stop,
-    ClipPath,
-    Pattern,
-    Mask,
-} from 'react-native-svg';
+
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Menu from "../assets/svgs/Vectormenu.svg"
 
 const List = () => {
     return (
         <View>
             <Text>List</Text>
-            <Menu width={120} height={40} />
-            <Svg height="50%" width="50%" viewBox="0 0 100 100">
-                <Circle
-                    cx="50"
-                    cy="50"
-                    r="45"
-                    stroke="blue"
-                    strokeWidth="2.5"
-                    fill="green"
-                />
-                <Rect
-                    x="15"
-                    y="15"
-                    width="70"
-                    height="70"
-                    stroke="red"
-                    strokeWidth="2"
-                    fill="yellow"
-                />
-            </Svg>
         </View>
     )
 }
