b0894ce5dfec7cef5ca3deb96867b69ea00ab609
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2022-06-03 14:39:58 +0300

    splash screen added

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './src/navigation/Router';
 import { Provider } from "react-redux";
 import { Store } from "./src/redux/Store";
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+
+
+  useEffect(() => {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }, [])
+
   return (
     <Provider store={Store}>
       <Router />
